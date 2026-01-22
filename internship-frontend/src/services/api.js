d4394604c3ff09920f4421f94a34035c7271c3b9
Shiva04-Dev <ganeshshiva772@gmail.com>
2026-01-23 00:11:01 +0200

    Corrected import of darkmode

--- internship-frontend/src/services/api.js
+++ internship-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5132/api';
+const API_BASE_URL = 'https://internshub.up.railway.app/api';
 
 //Creating axios instance with default config
 const api = axios.create({
